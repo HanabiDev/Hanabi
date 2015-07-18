cda9d150e8b27d21e294cc4f7b20800ed0f6dc4e
Juan Gómez <gomexa@hotmail.com>
2015-07-17 23:48:22 -0500

    Navbar updated

--- js/ct-navbar.js
+++ js/ct-navbar.js
@@ -46,8 +46,26 @@ $(document).scroll(function() {
     }
 });
 
++function ($) {
 
+    'use strict';
 
+    $("#navbar ul li a[href^='#']").on('click', function(e) {
+
+        e.preventDefault()
+        var hash = this.hash
+
+        $('html, body').animate(
+            {scrollTop: $(this.hash).offset().top}, 
+            1000, 
+            function(){
+                window.location.hash = hash
+            }
+        );
+
+    });
+
+}(jQuery);
 
 
 
