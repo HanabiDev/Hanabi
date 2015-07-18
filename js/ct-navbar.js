23bb9b674fe6e8f07844e505993edb1a3cd2526b
Juan Gómez <gomexa@hotmail.com>
2015-07-17 22:27:11 -0500

    Navbar updated

---
+++ js/ct-navbar.js
@@ -0,0 +1,60 @@
+searchVisible = 0;
+transparent = true;
+hasTransparent = false;
+
+$(document).ready(function(){
+   if($('nav[role="navigation"]').hasClass('navbar-transparent')){
+        hasTransparent = true;
+   }
+   $('[data-toggle="search"]').click(function(){
+        if(searchVisible == 0){
+            searchVisible = 1;
+            $(this).parent().addClass('active');
+            $(this).children('p').html('Close');
+            $('.navbar-search-form').fadeIn(function(){
+                $('.navbar-search-form input').focus();
+            });
+        } else {
+            searchVisible = 0;
+            $(this).parent().removeClass('active');
+            $(this).children('p').html('Search');
+            $(this).blur();
+            $('.navbar-search-form').fadeOut(function(){
+                $('.navbar-search-form input').blur();
+            });
+        } 
+    });
+
+    $('[data-toggle="tooltip"]').tooltip() 
+});
+
+$(document).scroll(function() {
+   if(hasTransparent){
+        if( $(this).scrollTop() > 590 ) {
+            if(transparent) {
+                transparent = false;
+                $('nav[role="navigation"]').removeClass('navbar-transparent');
+                $('.logo img').attr('src','images/logo_dark.png');
+            }
+        } else {
+            if( !transparent ) {
+                transparent = true;
+                $('nav[role="navigation"]').addClass('navbar-transparent');
+                $('.logo img').attr('src','images/logo.png');
+            }
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
